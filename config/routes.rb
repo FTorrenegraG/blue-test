976731cb1ba029fbe7372d9e564e8c307c03217d
Felix A Torrenegra G <f.torrenegrag@gmail.com>
2022-12-12 18:22:13 -0500

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  resources :page_urls, only: [:create, :index]
+  get ':id', to: 'page_urls#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
 end
